2f079adc21adc533a14c38530325e9e13ce7bed2
tianluxing <tianlx@bst2771.net>
2019-06-19 16:24:14 +0800

    新增greendao工具类

--- app/src/main/java/component/shine/com/basemoudle/MainActivity.java
+++ app/src/main/java/component/shine/com/basemoudle/MainActivity.java
@@ -3,6 +3,8 @@ package component.shine.com.basemoudle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import component.shine.com.basemoudle.utils.DaoManager;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //测试git
         //测试啦
+        DaoManager.getInstance().getDaoSession().getUserDao().insert(new User(null, "1", "2", "3", "4"));
     }
 }
