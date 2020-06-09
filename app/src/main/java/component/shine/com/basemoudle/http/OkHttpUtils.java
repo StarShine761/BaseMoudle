2ab3ee76c7f0d02fd96f03bf60697baa2e85f9ce
StarShine761 <xinggung761@gmail.com>
2020-06-09 15:08:38 +0800

    okhttp添加SSL无证书验证 添加国密SM3工具类

--- app/src/main/java/component/shine/com/basemoudle/http/OkHttpUtils.java
+++ app/src/main/java/component/shine/com/basemoudle/http/OkHttpUtils.java
@@ -1,8 +1,15 @@
 package component.shine.com.basemoudle.http;
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -10,6 +17,7 @@ import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -54,6 +62,8 @@ public class OkHttpUtils {
                 .readTimeout(10, TimeUnit.SECONDS)//设置读取超时时间
                 .writeTimeout(10, TimeUnit.SECONDS)//设置写入超时时间
                 .addInterceptor(new LoggingInterceptor())//添加日志拦截器，查看请求的url、参数、返回的结果
+                .sslSocketFactory(createSSLSocketFactory())
+                .hostnameVerifier((hostname, session) -> true)
                 .build();
     }
 
@@ -103,6 +113,75 @@ public class OkHttpUtils {
 
 
     }
+
+    /**
+     * @param url
+     * @param json
+     * @param reqCallBack post 提交json的形式
+     */
+    public void doPostJson(final String url, final String json, final ReqCallBack<String> reqCallBack) {
+
+
+        RequestBody requestBody = FormBody.create(MediaType.parse("application/json; charset=utf-8")
+                , json);
+
+        Request request = new Request.Builder()
+                .post(requestBody)
+                .url(url)
+                .build();
+
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+                reqCallBack.onError(e.toString());
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //保险
+                if (response != null & response.isSuccessful()) {
+                    final String strJson = response.body().string();
+                    reqCallBack.OnSuccess(strJson);
+
+                }
+            }
+        });
+
+
+    }
+
+    static class TrustAllCerts implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+
+    private static SSLSocketFactory createSSLSocketFactory() {
+        SSLSocketFactory ssfFactory = null;
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, new TrustManager[]{new TrustAllCerts()}, new SecureRandom());
+
+            ssfFactory = sc.getSocketFactory();
+        } catch (Exception e) {
+        }
+
+        return ssfFactory;
+    }
+
 
 
 }
